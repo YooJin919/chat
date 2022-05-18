1474e36607fe9c519f524ccf6966ec8efeeb2a31
YooJin919 <dbwls30131@naver.com>
2022-05-18 18:11:15 +0900

    6:11

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,6 +3,13 @@ const socket = io();
 const start = document.getElementById("st");
 const start_button = start.querySelector("button");
 
+
+function handleNickname(event){
+    event.preventDefault();
+    console.log('receive nickanem');
+}
+socket.on("nickname", handleNickname);
+
 function handleStart(event){
     event.preventDefault();
     window.location.replace(`http://localhost:3000/test`); // page 이동
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import http from "http";
 import socketIO from "socket.io";
+import { createBrotliCompress } from "zlib";
 
 // 같은 server에서 http, websocket 서버 2개 돌림
 const app = express();
@@ -31,30 +32,29 @@ let user2 = {
     Id: "",
 };
 
-
-
-// split(" ") 함수를 사용해서 roomId -> 공백기준으로 nickname 2개 가져올 수 있음 == 문자열을 배열로 쪼개줌
-// let name = roomId.split(" ");
-// console.log('name[0] : ',name[0]); // dongha
-// console.log('name[1] : ', name[1]); // jin
-
-
-
 // Socket room을 만드는 Id = 사용자 2명의 nickname
 let roomId = '';
 
+
+
+function createRoom (roomId) {
+    socket.emit("nickname", roomId);
+    //socket.join(roomId);
+
+}
+
 wsServer.on("connection", (socket) => {
     console.log("Connected to Web Socket Server!");
-    socket.on("enterRoom", (nickname)=>{
-        roomId = nickname;
-        console.log('socket.on createRoom!');
-        console.log(roomId);
+    // socket.on("enterRoom", (nickname)=>{
+    //     roomId = nickname;
+    //     console.log('socket.on createRoom!');
+    //     console.log(roomId);
 
-        console.log('socker.rooms : ',socket.rooms);
-        console.log('socket.id (before join) : ',socket.id);
-        socket.join(roomId); // room 생성
-        console.log('socket.id (after join) : ',socket.rooms);
-    });
+    //     console.log('socker.rooms : ',socket.rooms);
+    //     console.log('socket.id (before join) : ',socket.id);
+    //     socket.join(roomId); // room 생성
+    //     console.log('socket.id (after join) : ',socket.rooms);
+    // });
 });
 
 
@@ -78,23 +78,32 @@ const get_user_info = async (req, res) => {
         }
         console.log('user1 : ', user1, ' user2 : ',user2);
         roomId = user1.nickname + " " + user2.nickname;
-        console.log('roomId : ', roomId);
+        createRoom(roomId);
         //res.send(roomId); // front 화면에 보냄
         res.render("home");
-        //CreateRoom(roomId); // 가져온 user_Id로 room 생성하기
     } catch(err){
         console.log(err);
         res.send(err);
     }
 }
 
-app.get("/test", get_user_info); // spring에서 request 받기
-// app.post("/test", (req, res)=>{
-//     const {
-//         body : { name },
-//     }=req;
-//     res.send(`${name}`);
-// })
+app.get('/test', get_user_info);
+
+// // 매칭된 2명의 사용자 nickname 받기
+// app.post("http://3.39.141.110:8080/user/nickname",(req, res)=>{
+//     // req.body = {
+//     //     "user1" : "nickname",
+//     //     "user2" : "nickname"
+//     // }
+//     console.log(req.body);
+
+//     user1.nickname=req.body.user1;
+//     user2.nickname=req.body.user2;
+//     roomId = user1.nickname + " " + user2.nickname;
+//     get_user_info();
+//     createRoom(roomId); // 채팅방 생성
+//     res.send(user);
+// } )
 
 
 const handleListen = () => {
